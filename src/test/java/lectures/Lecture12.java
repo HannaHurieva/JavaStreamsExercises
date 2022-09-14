03efacdcf244f173d821d0ed5518d7720e9d0645
HHURIEVA <Hanna.Hurieva@davita.com>
2022-09-14 15:16:06 +0300

    Lecture12: collectors with streams

--- src/test/java/lectures/Lecture12.java
+++ src/test/java/lectures/Lecture12.java
@@ -1,20 +1,35 @@
 package lectures;
 
 import beans.Person;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
-public class Lecture12 {
-  @Test
-  public void understandingCollect() throws Exception {
-    List<String> emails = MockData.getPeople()
-        .stream()
-        .map(Person::getEmail)
-        .collect(Collectors.toList());
+import static org.assertj.core.api.Assertions.assertThat;
 
-    emails.forEach(System.out::println);
-  }
+public class Lecture12 {
+    @Test
+    public void understandingCollect() throws Exception {
+        List<String> emails = MockData.getPeople()
+                .stream()
+                .map(Person::getEmail)
+                .collect(() -> new ArrayList<String>(),
+                        (list, element) -> list.add(element),
+                        (list1, list2) -> list1.addAll(list2));
+        //.collect(Collectors.toList());
+
+        List<String> emailsLambda = MockData.getPeople()
+                .stream()
+                .map(Person::getEmail)
+                .collect(ArrayList::new,
+                        ArrayList::add,
+                        ArrayList::addAll);
+
+        //emails.forEach(System.out::println);
+        assertThat(emails).isEqualTo(emailsLambda);
+    }
 }
