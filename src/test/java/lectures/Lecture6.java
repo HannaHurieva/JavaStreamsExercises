b2a2ba3a3f2206f435f2dd7146fa508341ebfaba
HHURIEVA <Hanna.Hurieva@davita.com>
2022-09-14 12:40:32 +0300

    Lecture6: predicate changed in the tests

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -16,21 +16,30 @@ public class Lecture6 {
     @Test
     public void findAny() throws Exception {
         Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
         int any = Arrays.stream(numbers)
-                .filter(n -> n < 10)
+                .filter(numbersLessThan10)
                 .findAny()
                 .orElse(0);
 
         System.out.println(any);
-        assertThat(any).isEqualTo(1);
+        //nondeterministic
+        assertThat(any).isEqualTo(6);
     }
 
     @Test
     public void findFirst() throws Exception {
         Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        Optional<Integer> first = Arrays.stream(numbers).findFirst();
+        Optional<Integer> first = Arrays.stream(numbers).
+                filter(numbersLessThan10)
+                .findFirst();
 
-        assertThat(first.get()).isEqualTo(1);
+        int result = 0;
+        if (first.isPresent()) {
+            result = first.get();
+        }
+        //deterministic
+        assertThat(result).isEqualTo(6);
     }
 }
 
