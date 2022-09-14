63abce0cddc8e3558441726c0ea1bec55fb39709
HHURIEVA <Hanna.Hurieva@davita.com>
2022-09-14 15:15:46 +0300

    Lecture11: joining with streams

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -1,22 +1,36 @@
 package lectures;
 
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Lecture11 {
 
-  @Test
-  public void joiningStrings() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    private static final String expected = "Anna, John, Marcos, Helena, Yasmin";
 
-  }
+    @Test
+    public void joiningStrings() throws Exception {
+        List<String> names = ImmutableList.of("Anna", "John", "Marcos", "Helena", "Yasmin");
+        String listOfNames = String.join(", ", names);
+        System.out.println(listOfNames);
 
-  @Test
-  public void joiningStringsWithStream() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        assertThat(listOfNames).isEqualToIgnoringCase(expected);
+    }
 
-  }
+    @Test
+    public void joiningStringsWithStream() throws Exception {
+        List<String> names = ImmutableList.of("Anna", "John", "Marcos", "Helena", "Yasmin");
+
+        String join = names.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.joining("|"));
+        System.out.println(join);
+
+    }
 }
