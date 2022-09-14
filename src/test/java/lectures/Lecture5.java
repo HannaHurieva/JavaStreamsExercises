97ad346eff785aaa9b482764113d75f09ab9586c
HHURIEVA <Hanna.Hurieva@davita.com>
2022-09-14 11:54:33 +0300

    Lecture5: filter with Predicate

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -7,37 +7,49 @@ import beans.Car;
 import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture5 {
 
-  @Test
-  public void understandingFilter() throws Exception {
-    ImmutableList<Car> cars = MockData.getCars();
+    final Predicate<Car> carPredicate = car -> car.getPrice() < 20000;
 
-  }
+    @Test
+    public void understandingFilter() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
 
-  @Test
-  public void ourFirstMapping() throws Exception {
-    // transform from one data type to another
-    List<Person> people = MockData.getPeople();
+        List<Car> carsFiltered = cars.stream()
+                .filter(carPredicate)
+                .collect(Collectors.toList());
 
-  }
+        //carsFiltered.forEach(System.out::println);
+        System.out.println(carsFiltered.size());
 
-  @Test
-  public void averageCarPrice() throws Exception {
-    // calculate average of car prices
+        assertThat(carsFiltered).hasSize(156);
+    }
 
-  }
+    @Test
+    public void ourFirstMapping() throws Exception {
+        // transform from one data type to another
+        List<Person> people = MockData.getPeople();
 
-  @Test
-  public void test() throws Exception {
+    }
 
-  }
+    @Test
+    public void averageCarPrice() throws Exception {
+        // calculate average of car prices
+
+    }
+
+    @Test
+    public void test() throws Exception {
+
+    }
 }
 
 
