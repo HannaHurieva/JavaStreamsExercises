2852ff263976b576e294a9ba26a3431a55511aeb
HHURIEVA <Hanna.Hurieva@davita.com>
2022-09-14 12:09:43 +0300

    Lecture5: average price of cars with stream.mapToDouble

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -1,19 +1,20 @@
 package lectures;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import beans.Car;
 import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
+import mockdata.MockData;
+import org.junit.Test;
 
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import mockdata.MockData;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Lecture5 {
 
@@ -38,11 +39,12 @@ public class Lecture5 {
         // transform from one data type to another
         List<Person> people = MockData.getPeople();
 
+        Function<Person, PersonDTO> personPersonDTOFunction = person -> new PersonDTO(person.getId(),
+                person.getFirstName(), person.getAge());
+
         List<PersonDTO> dtos = people.stream()
-                .map(person -> {
-                    PersonDTO dto = new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
-                    return dto;
-                })
+                .map(PersonDTO::map)
+                //.map(personPersonDTOFunction)
                 .collect(Collectors.toList());
 
         //dtos.forEach(System.out::println);
@@ -54,7 +56,12 @@ public class Lecture5 {
     @Test
     public void averageCarPrice() throws Exception {
         // calculate average of car prices
+        double averagePrice = MockData.getCars().stream()
+                .mapToDouble(Car::getPrice)
+                .average()
+                .orElse(0);
 
+        System.out.println("Average price of cars: " + averagePrice);
     }
 
     @Test
