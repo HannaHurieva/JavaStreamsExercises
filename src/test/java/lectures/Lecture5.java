5c16deca04ff449e68ee82df25ed622a50269153
HHURIEVA <Hanna.Hurieva@davita.com>
2022-09-14 12:33:37 +0300

    Lecture6: findAny and findFirst with stream

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -9,7 +9,6 @@ import mockdata.MockData;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.OptionalDouble;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -1,25 +1,36 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
-import java.util.Arrays;
-import java.util.function.Predicate;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Lecture6 {
 
-  final Predicate<Integer> numbersLessThan10 = n -> n > 5 && n < 10;
+    final Predicate<Integer> numbersLessThan10 = n -> n > 5 && n < 10;
 
-  @Test
-  public void findAny() throws Exception {
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    @Test
+    public void findAny() throws Exception {
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int any = Arrays.stream(numbers)
+                .filter(n -> n < 10)
+                .findAny()
+                .orElse(0);
 
-  }
+        System.out.println(any);
+        assertThat(any).isEqualTo(1);
+    }
 
-  @Test
-  public void findFirst() throws Exception {
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    @Test
+    public void findFirst() throws Exception {
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Optional<Integer> first = Arrays.stream(numbers).findFirst();
 
-  }
+        assertThat(first.get()).isEqualTo(1);
+    }
 }
 
