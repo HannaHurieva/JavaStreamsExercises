e4a2a21fee619623ce6b995208f771121b340847
HHURIEVA <Hanna.Hurieva@davita.com>
2022-09-14 14:43:35 +0300

    Lecture9: reduce with streams

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -1,17 +1,25 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
-import java.util.Arrays;
 import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Lecture9 {
 
-  @Test
-  public void reduce() throws Exception {
-    Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+    @Test
+    public void reduce() throws Exception {
+        Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
 
-  }
+        int sum = Arrays.stream(integers)
+                //.reduce(0, (a, b) -> a + b);
+                .reduce(0, Integer::sum);
+        System.out.println(sum);
+
+        assertThat(sum).isEqualTo(1831);
+    }
 
 
 }
