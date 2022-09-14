4550e9fcc14fcdb30bc991fdc99f4dab196d1a11
HHURIEVA <Hanna.Hurieva@davita.com>
2022-09-14 14:51:43 +0300

    Lecture10: flatMap with streams

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -2,36 +2,56 @@ package lectures;
 
 
 import com.google.common.collect.Lists;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Lecture10 {
 
-  private static final List<ArrayList<String>> arrayListOfNames = Lists.newArrayList(
-      Lists.newArrayList("Mariam", "Alex", "Ismail"),
-      Lists.newArrayList("John", "Alesha", "Andre"),
-      Lists.newArrayList("Susy", "Ali")
-  );
+    private static final List<ArrayList<String>> arrayListOfNames = Lists.newArrayList(
+            Lists.newArrayList("Mariam", "Alex", "Ismail"),
+            Lists.newArrayList("John", "Alesha", "Andre"),
+            Lists.newArrayList("Susy", "Ali")
+    );
 
-  @Before
-  public void setUp() {
-    System.out.println(arrayListOfNames);
-  }
+    private static final List<String> expected = Lists.newArrayList("Mariam", "Alex", "Ismail",
+            "John", "Alesha", "Andre", "Susy", "Ali");
 
-  @Test
-  public void withoutFlatMap() throws Exception {
+    @Before
+    public void setUp() {
+        System.out.println(arrayListOfNames);
+    }
+
+    @Test
+    public void withoutFlatMap() throws Exception {
 //    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+        List<String> names = new ArrayList<>();
 
-  }
+        for (List<String> list : arrayListOfNames) {
+            names.addAll(list);
+        }
+        System.out.println(names);
 
-  @Test
-  public void withFlatMap() throws Exception {
+        assertThat(names).hasSize(8);
+        assertThat(names).isEqualTo(expected);
+    }
+
+    @Test
+    public void withFlatMap() throws Exception {
 //   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+        List<String> names = arrayListOfNames.stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+        System.out.println(names);
 
-  }
+        assertThat(names).hasSize(8);
+        assertThat(names).isEqualTo(expected);
+    }
 
 }
 
